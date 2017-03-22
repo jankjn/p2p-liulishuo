62d27562948497232bde12b569d2943274dd5bb3
zzz <jankin.z@outlook.com>
2017-03-22 16:38:56 +0800

    set default format to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  resources :loans, only: [:index, :show, :create] do
-    post 'confirm', on: :member
-  end
-  resources :pay_backs, only: [:index, :show, :create]
-  resources :accounts, only: [:index, :show, :create]
+  defaults format: :json do
+    resources :loans, only: [:index, :show, :create] do
+      post 'confirm', on: :member
+    end
+    resources :pay_backs, only: [:index, :show, :create]
+    resources :accounts, only: [:index, :show, :create]
 
-  post 'sign_up', to: 'accounts#create'
-  post 'login', to: 'sessions#create'
+    post 'sign_up', to: 'accounts#create'
+    post 'login', to: 'sessions#create'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
