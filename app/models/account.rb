bdd90bb96cff17a040927ea38dce828459ef89bd
zzz <jankin.z@outlook.com>
2017-03-22 13:34:02 +0800

    add debt_status in account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,27 +2,38 @@ class Account < ApplicationRecord
 
   validates :deposit, numericality: { greater_than_or_equal_to: 0 }
 
+  def debt_status
+    { borrows: borrows, lends: lends, deposit: deposit }
+  end
+
+  def debt_status_with(account)
+    {
+      borrows: borrows_from(account),
+      lends: lends_to(account),
+    }
+  end
+
   def borrows
     transaction do
-      Loan.where(borrower: self).sum(:amount) - PayBack.where(borrower: self).sum(:amount)
+      Loan.where(borrower: self, confirmed: true).sum(:amount) - PayBack.where(borrower: self).sum(:amount)
     end
   end
 
   def lends
     transaction do
-      Loan.where(lender: self).sum(:amount) - PayBack.where(lender: self).sum(:amount)
+      Loan.where(lender: self, confirmed: true).sum(:amount) - PayBack.where(lender: self).sum(:amount)
     end
   end
 
   def borrows_from(lender)
     transaction do
-      Loan.where(borrower: self, lender: lender).sum(:amount) - PayBack.where(borrower: self, lender: lender).sum(:amount)
+      Loan.where(borrower: self, lender: lender, confirmed: true).sum(:amount) - PayBack.where(borrower: self, lender: lender).sum(:amount)
     end
   end
 
   def lends_to(borrower)
     transaction do
-      Loan.where(borrower: borrower, lender: self).sum(:amount) - PayBack.where(borrower: borrower, lender: self).sum(:amount)
+      Loan.where(borrower: borrower, lender: self, confirmed: true).sum(:amount) - PayBack.where(borrower: borrower, lender: self).sum(:amount)
     end
   end
 
