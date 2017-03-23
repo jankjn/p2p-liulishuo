132ecfddc226c844ccb25c019684d8cef34e228e
zzz <jankin.z@outlook.com>
2017-03-23 15:09:21 +0800

    loan error handling

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -8,32 +8,44 @@ class LoansController < ApplicationController
   end
 
   def show
-    render json: Loan.find(params[:id])
+    loan = Loan.find_by(id: params[:id])
+    if loan
+      render json: loan
+    else
+      render json: { error: 'loan not found' }, status: 404
+    end
   end
 
   def create
-    lender = Account.find(create_params[:lender_id])
-    borrower = Account.find(create_params[:borrower_id])
+    return if miss_params(:lender_id, :borrower_id, :amount)
+
+    lender = Account.find_by(id: create_params[:lender_id])
+    borrower = Account.find_by(id: create_params[:borrower_id])
     loan = Loan.new(lender: lender, borrower: borrower, amount: create_params[:amount])
 
-    if loan.overflow?
+    if lender.nil?
+      render json: { error: 'lender is not found' }, status: 404
+    elsif borrower.nil?
+      render json: { error: 'borrower is not found' }, status: 404
+    elsif loan.overflow?
       render json: { error: 'money not enough to lend' }, status: 400
     else
-      loan.save!
-      render json: loan
+      loan.save! and render json: loan
     end
   end
 
   def confirm
-    loan = Loan.find(params[:id])
+    loan = Loan.find_by(id: params[:id])
 
-    if @current_user == loan.lender
+    if loan.nil?
+      render json: { error: 'loan is invalid' }, status: 404
+    elsif @current_user == loan.lender
       ConfirmLoanAction.exec(loan)
       render json: loan
     else
       render json: { error: 'only lender can confirm a loan' }, status: 403
     end
-  rescue
+  rescue LoanOverflowError
     render json: { error: 'money not enough to lend' }, status: 400
   end
 
